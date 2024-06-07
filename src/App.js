fdb8167db2605b0b1b13f44d25607b07e423ec0c
Pulkit-Ahuja <pulkitahuja2431@gmail.com>
2024-06-07 12:42:10 +0530

    another commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a React App.
+          This is just another React App.
         </p>
         <a
           className="App-link"
